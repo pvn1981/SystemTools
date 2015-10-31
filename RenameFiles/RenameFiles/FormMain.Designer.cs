cfdf7c63ca059f68abebb8bbce50df9299042788
Popov Vladimir <pvn1981@qip.ru>
2015-10-31 10:57:52 +0300

    Добавил приложение переименовывающее файлы мультфильма Лунтик

---
+++ RenameFiles/RenameFiles/FormMain.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace RenameFiles
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFolderName = new System.Windows.Forms.TextBox();
+            this.buttonChoose = new System.Windows.Forms.Button();
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxFolderName
+            // 
+            this.textBoxFolderName.Location = new System.Drawing.Point(23, 12);
+            this.textBoxFolderName.Name = "textBoxFolderName";
+            this.textBoxFolderName.Size = new System.Drawing.Size(364, 20);
+            this.textBoxFolderName.TabIndex = 0;
+            // 
+            // buttonChoose
+            // 
+            this.buttonChoose.Location = new System.Drawing.Point(393, 10);
+            this.buttonChoose.Name = "buttonChoose";
+            this.buttonChoose.Size = new System.Drawing.Size(75, 23);
+            this.buttonChoose.TabIndex = 1;
+            this.buttonChoose.Text = "Выбрать";
+            this.buttonChoose.UseVisualStyleBackColor = true;
+            this.buttonChoose.Click += new System.EventHandler(this.buttonChoose_Click);
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.Location = new System.Drawing.Point(479, 9);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(75, 23);
+            this.buttonApply.TabIndex = 2;
+            this.buttonApply.Text = "Применить";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 44);
+            this.Controls.Add(this.buttonApply);
+            this.Controls.Add(this.buttonChoose);
+            this.Controls.Add(this.textBoxFolderName);
+            this.Name = "FormMain";
+            this.Text = "Переименовать файлы";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFolderName;
+        private System.Windows.Forms.Button buttonChoose;
+        private System.Windows.Forms.Button buttonApply;
+    }
+}
+
---
+++ RenameFiles/RenameFiles/FormMain.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenameFiles
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonChoose_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog diag = new FolderBrowserDialog();
+            diag.SelectedPath = textBoxFolderName.Text;
+
+            if (diag.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string folderPath = diag.SelectedPath;
+
+                    textBoxFolderName.Text = folderPath;
+
+                    Properties.Settings.Default.OldPath = folderPath;
+                    Properties.Settings.Default.Save();
+
+                    DirectoryInfo d = new DirectoryInfo(folderPath);
+                    FileInfo[] infos = d.GetFiles();
+
+                    foreach (FileInfo f in infos)
+                    {
+                        string fileName = f.Name;
+                        int index = fileName.IndexOf(". ");
+
+                        string numberString = fileName.Substring(0, index);
+
+                        int allLength = fileName.Length;
+                        int nameLength = allLength - (index + 2);
+
+                        string namePlusExtantion = fileName.Substring(index + 2, nameLength);
+                        int number = Convert.ToInt32(numberString);
+
+                        string newFileName = number.ToString("000") + "_" + namePlusExtantion;
+                        string newFullFileName = folderPath + "\\" + newFileName;
+
+                        File.Move(f.FullName, newFullFileName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            textBoxFolderName.Text = Properties.Settings.Default.OldPath;
+        }
+    }
+}
