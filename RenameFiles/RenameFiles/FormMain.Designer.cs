ac66caaf36a50dc63325caf04b6763a720181938
pvn1981 <pvn1981@gmail.com>
2015-11-03 11:18:36 +0300

    Провел рефакторинг кода Добавил ассинхронность Добавил Progress

--- RenameFiles/RenameFiles/FormMain.Designer.cs
+++ RenameFiles/RenameFiles/FormMain.Designer.cs
@@ -31,18 +31,21 @@
             this.textBoxFolderName = new System.Windows.Forms.TextBox();
             this.buttonChoose = new System.Windows.Forms.Button();
             this.buttonApply = new System.Windows.Forms.Button();
+            this.labelChoosePlugin = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.progressBarMain = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // textBoxFolderName
             // 
-            this.textBoxFolderName.Location = new System.Drawing.Point(23, 12);
+            this.textBoxFolderName.Location = new System.Drawing.Point(12, 54);
             this.textBoxFolderName.Name = "textBoxFolderName";
             this.textBoxFolderName.Size = new System.Drawing.Size(364, 20);
             this.textBoxFolderName.TabIndex = 0;
             // 
             // buttonChoose
             // 
-            this.buttonChoose.Location = new System.Drawing.Point(393, 10);
+            this.buttonChoose.Location = new System.Drawing.Point(382, 52);
             this.buttonChoose.Name = "buttonChoose";
             this.buttonChoose.Size = new System.Drawing.Size(75, 23);
             this.buttonChoose.TabIndex = 1;
@@ -52,18 +55,46 @@
             // 
             // buttonApply
             // 
-            this.buttonApply.Location = new System.Drawing.Point(479, 9);
+            this.buttonApply.Location = new System.Drawing.Point(468, 51);
             this.buttonApply.Name = "buttonApply";
             this.buttonApply.Size = new System.Drawing.Size(75, 23);
             this.buttonApply.TabIndex = 2;
             this.buttonApply.Text = "Применить";
             this.buttonApply.UseVisualStyleBackColor = true;
+            this.buttonApply.Click += new System.EventHandler(this.buttonApply_Click);
+            // 
+            // labelChoosePlugin
+            // 
+            this.labelChoosePlugin.AutoSize = true;
+            this.labelChoosePlugin.Location = new System.Drawing.Point(9, 21);
+            this.labelChoosePlugin.Name = "labelChoosePlugin";
+            this.labelChoosePlugin.Size = new System.Drawing.Size(87, 13);
+            this.labelChoosePlugin.TabIndex = 3;
+            this.labelChoosePlugin.Text = "Выбор плагина:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(114, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(429, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // progressBarMain
+            // 
+            this.progressBarMain.Location = new System.Drawing.Point(12, 94);
+            this.progressBarMain.Name = "progressBarMain";
+            this.progressBarMain.Size = new System.Drawing.Size(531, 23);
+            this.progressBarMain.TabIndex = 5;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(566, 44);
+            this.ClientSize = new System.Drawing.Size(566, 129);
+            this.Controls.Add(this.progressBarMain);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelChoosePlugin);
             this.Controls.Add(this.buttonApply);
             this.Controls.Add(this.buttonChoose);
             this.Controls.Add(this.textBoxFolderName);
@@ -80,6 +111,9 @@
         private System.Windows.Forms.TextBox textBoxFolderName;
         private System.Windows.Forms.Button buttonChoose;
         private System.Windows.Forms.Button buttonApply;
+        private System.Windows.Forms.Label labelChoosePlugin;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ProgressBar progressBarMain;
     }
 }
 
--- RenameFiles/RenameFiles/FormMain.cs
+++ RenameFiles/RenameFiles/FormMain.cs
@@ -13,13 +13,46 @@ namespace RenameFiles
 {
     public partial class FormMain : Form
     {
+
+        #region Constructor
+
         public FormMain()
         {
             InitializeComponent();
         }
 
+        #endregion
+
+        #region Event handler
+
         private void buttonChoose_Click(object sender, EventArgs e)
         {
+            string folderPath = ChooseFolder();
+
+            textBoxFolderName.Text = folderPath;
+
+            Properties.Settings.Default.OldPath = folderPath;
+            Properties.Settings.Default.Save();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            textBoxFolderName.Text = Properties.Settings.Default.OldPath;
+        }
+
+        async private void buttonApply_Click(object sender, EventArgs e)
+        {
+            await Rename(Properties.Settings.Default.OldPath);
+        }
+
+        #endregion
+
+        #region Methods
+
+        string ChooseFolder()
+        {
+            StringBuilder sb = new StringBuilder();
+
             FolderBrowserDialog diag = new FolderBrowserDialog();
             diag.SelectedPath = textBoxFolderName.Text;
 
@@ -27,45 +60,61 @@ namespace RenameFiles
             {
                 try
                 {
-                    string folderPath = diag.SelectedPath;
-
-                    textBoxFolderName.Text = folderPath;
-
-                    Properties.Settings.Default.OldPath = folderPath;
-                    Properties.Settings.Default.Save();
-
-                    DirectoryInfo d = new DirectoryInfo(folderPath);
-                    FileInfo[] infos = d.GetFiles();
-
-                    foreach (FileInfo f in infos)
-                    {
-                        string fileName = f.Name;
-                        int index = fileName.IndexOf(". ");
-
-                        string numberString = fileName.Substring(0, index);
-
-                        int allLength = fileName.Length;
-                        int nameLength = allLength - (index + 2);
-
-                        string namePlusExtantion = fileName.Substring(index + 2, nameLength);
-                        int number = Convert.ToInt32(numberString);
-
-                        string newFileName = number.ToString("000") + "_" + namePlusExtantion;
-                        string newFullFileName = folderPath + "\\" + newFileName;
-
-                        File.Move(f.FullName, newFullFileName);
-                    }
+                    sb.Append(diag.SelectedPath);
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
                 }
             }
+
+            return sb.ToString();
         }
 
-        private void FormMain_Load(object sender, EventArgs e)
+        async Task Rename(string folderPath)
         {
-            textBoxFolderName.Text = Properties.Settings.Default.OldPath;
+            await Task.Run(() =>
+            {
+                DirectoryInfo d = new DirectoryInfo(folderPath);
+                FileInfo[] infos = d.GetFiles("*.mkv");
+
+                //  Названия в папке: "1. Фотографии.mkv", 
+                //                    "84. Ветка.mkv", 
+                //                    "101. Песня ветра.mkv"
+                //
+                //  Должно быть:      "001_Фотографии.mkv" итд
+
+                progressBarMain.Minimum = 0;
+                progressBarMain.Maximum = infos.Count();
+
+                foreach (FileInfo f in infos)
+                {
+                    string fileName = f.Name;
+
+                    string patternSearch = ". ";
+                    int patternLength = patternSearch.Length;
+
+                    int startPatternAddress = fileName.IndexOf(patternSearch);
+                    int endPatternAddress = startPatternAddress + patternLength;
+
+                    string numberString = fileName.Substring(0, startPatternAddress);
+
+                    int allLength = fileName.Length;
+                    int nameLength = allLength - endPatternAddress;
+
+                    string namePlusExtention = fileName.Substring(endPatternAddress, nameLength);
+                    int number = Convert.ToInt32(numberString);
+
+                    string newFileName = number.ToString("000") + "_" + namePlusExtention;
+                    string newFullFileName = folderPath + "\\" + newFileName;
+
+                    File.Move(f.FullName, newFullFileName);
+
+                    progressBarMain.Value += 1;
+                }
+            });
         }
+
+        #endregion
     }
 }
