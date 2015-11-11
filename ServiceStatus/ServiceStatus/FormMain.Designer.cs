6c3ab6c583ceb1f2f20394a9f6878595ce0f825d
pvn1981 <pvn1981@gmail.com>
2015-11-11 19:10:11 +0300

    Добавил приложение для просмотра служб (выделяется служба времени Windows)

---
+++ ServiceStatus/ServiceStatus/FormMain.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ServiceStatus
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.listBoxServices = new System.Windows.Forms.ListBox();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonfind = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelMain = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(260, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // listBoxServices
+            // 
+            this.listBoxServices.FormattingEnabled = true;
+            this.listBoxServices.Location = new System.Drawing.Point(12, 12);
+            this.listBoxServices.Name = "listBoxServices";
+            this.listBoxServices.Size = new System.Drawing.Size(223, 277);
+            this.listBoxServices.TabIndex = 2;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(260, 50);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(260, 91);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 4;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonfind
+            // 
+            this.buttonfind.Location = new System.Drawing.Point(260, 135);
+            this.buttonfind.Name = "buttonfind";
+            this.buttonfind.Size = new System.Drawing.Size(75, 23);
+            this.buttonfind.TabIndex = 5;
+            this.buttonfind.Text = "Find";
+            this.buttonfind.UseVisualStyleBackColor = true;
+            this.buttonfind.Click += new System.EventHandler(this.buttonfind_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelMain});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 320);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(350, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelMain
+            // 
+            this.toolStripStatusLabelMain.Name = "toolStripStatusLabelMain";
+            this.toolStripStatusLabelMain.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabelMain.Text = "toolStripStatusLabel1";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 342);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonfind);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.listBoxServices);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "FormMain";
+            this.Text = "Service status";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListBox listBoxServices;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonfind;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelMain;
+    }
+}
+
